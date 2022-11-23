a2363bcb63619fcb7af9257b90ce9ed54842d98d
hobakk <hobakk12@gmail.com>
2022-11-23 15:46:12 +0900

    MemoList 정검

--- Memo/src/MemoList.java
+++ Memo/src/MemoList.java
@@ -1,24 +1,64 @@
-
-
-public class MemoList {
-    /*
-     * 리스트 생성하고
-     * 정보 입력받으면
-     * setter 가 리스트 추가
-     * getter 가 리스트 내용 반환
-     */
+    public class MemoList {
+        private String m_User, m_Contents, m_Date;
+        private int m_PW;
+        private Memo allUsers;      // 객체간 상호관계
+        private int count = 0;              // 순번
+        MemoList arr[];
 
     public MemoList() {
-
+        arr = new MemoList[10];     // 배열 초기화
+    }
+    public MemoList(String m_User, String m_Contents, String m_Date, int m_PW) {
+        this.m_User = m_User;
+        this.m_Contents = m_Contents;
+        this.m_Date = m_Date;
+        this.m_PW = m_PW;
     }
 
-    // 목록 조회하기
-    public void Getter() {
-
+        // 목록 조회하기
+    public void setUser() {
+        this.m_User = m_User;
+    }
+    public void setContents() {
+        this.m_Contents = m_Contents;
+    }
+    public void setDate() {
+        this.m_Date = m_Date;
+    }
+    public void setPassword() {
+        this.m_PW = m_PW;
     }
 
-    // 메모 리스트 작성하기
-    public void Setter() {
 
+        // 메모 리스트 작성하기
+    public void getUser() {
+        return m_User;
+    }
+    public void getContents() {
+        return m_Contents;
+    }
+    public void getDate() {
+        return m_Date;
+    }
+    public void getPassword() {
+        return m_PW;
+    }
+
+        // List 출력문
+    public void printuserList() {
+        System.out.println(count+" ID: "+m_User+" PW:"+m_PW+" Memo: "+m_Contents+" Time: "+m_Date);
+    }
+        // count를 기준으로 검색
+    public void searchMemoList() {
+        // ?
+    }
+
+
+
+    public Memo getallUsers() {
+        return allUsers;
+    }
+    public void setAllUsers() {
+        return allUsers;
     }
 }
